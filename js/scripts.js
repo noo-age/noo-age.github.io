d21e69d2d92c0f7f7a4596627647c4c6b3aa40f0
Yejun <yejunyun@Yejuns-MBP.wireless.yale.internal>
2024-09-22 20:38:12 -0400

    init

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,13 @@
-// Set current year in footer
-document.addEventListener('DOMContentLoaded', () => {
-    const currentYearSpan = document.getElementById('currentYear');
-    if (currentYearSpan) {
-        currentYearSpan.textContent = new Date().getFullYear();
-    }
-});
-
-// Create stars
 function createStars() {
-    const starsContainer = document.querySelector('.stars');
     const numStars = 100;
-
+    const starBackground = document.querySelector('.star-background');
     for (let i = 0; i < numStars; i++) {
         const star = document.createElement('div');
-        star.classList.add('star');
+        star.className = 'star';
         star.style.left = `${Math.random() * 100}%`;
         star.style.top = `${Math.random() * 100}%`;
-        star.style.width = `${Math.random() * 3}px`;
-        star.style.height = star.style.width;
-        star.style.animationDelay = `${Math.random() * 2}s`;
-        starsContainer.appendChild(star);
+        star.style.animationDelay = `${Math.random() * 1}s`;
+        starBackground.appendChild(star);
     }
 }
-
-document.addEventListener('DOMContentLoaded', createStars);
+createStars();
